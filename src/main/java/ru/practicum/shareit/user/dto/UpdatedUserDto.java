ad5f62b77372e0a7e9acb38ed92d12b2a49a5bba
Nikita Savelyev <nsavelyev@it-aces.com>
2024-05-23 23:04:51 +0300

    style: исправление codestyle 2

--- src/main/java/ru/practicum/shareit/user/dto/UpdatedUserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UpdatedUserDto.java
@@ -2,10 +2,6 @@ package ru.practicum.shareit.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
